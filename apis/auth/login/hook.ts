7a6383c732eaf5df97f98c13a595714ce9f43682
Amirhossein Beigi <dev.amirhbeigi@gmail.com>
2022-04-15 12:53:36 +0430

    after login/register set user

--- apis/auth/login/hook.ts
+++ apis/auth/login/hook.ts
@@ -1,10 +1,15 @@
-import { useMutation } from "react-query";
-import { setCookies } from "cookies-next";
-import { login } from "./api";
+import { useMutation } from 'react-query';
+import { setCookies } from 'cookies-next';
+import { login } from './api';
+import { useUser } from '../../../contexts/user';
 
-export const useLogin = () =>
-  useMutation(login, {
-    onSuccess: (data) => {
-      setCookies("votely.token", data.data.access_token);
-    },
+export const useLogin = () => {
+  const [, setUser] = useUser();
+
+  return useMutation(login, {
+    onSuccess: data => {
+      setCookies('votely.token', data.data.access_token);
+      setUser(data.data.user);
+    }
   });
+};
--- apis/auth/register/hook.ts
+++ apis/auth/register/hook.ts
@@ -1,10 +1,15 @@
 import { useMutation } from 'react-query';
 import { setCookies } from 'cookies-next';
 import { register } from './api';
+import { useUser } from '../../../contexts/user';
 
-export const useRegister = () =>
-  useMutation(register, {
+export const useRegister = () => {
+  const [, setUser] = useUser();
+
+  return useMutation(register, {
     onSuccess: data => {
       setCookies('votely.token', data.data.access_token);
+      setUser(data.data.user);
     }
   });
+};
