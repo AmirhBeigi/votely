d3c146886f30f419f4c8f59a44d20f7247610c68
Amirhossein Beigi <dev.amirhbeigi@gmail.com>
2022-04-23 17:17:40 +0430

    remove bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const withPWA = require('next-pwa');
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true'
-});
 
 const nextConfig = {
   reactStrictMode: true,
@@ -14,4 +11,4 @@ const nextConfig = {
   }
 };
 
-module.exports = withPWA(withBundleAnalyzer(nextConfig));
+module.exports = withPWA(nextConfig);
