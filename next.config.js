abc37989b08b1b192fca566b044b77b966e1f19e
Amirhossein Beigi <dev.amirhbeigi@gmail.com>
2022-04-29 11:13:31 +0430

    dsaible pwa in dev environment

--- next.config.js
+++ next.config.js
@@ -1,13 +1,16 @@
 /** @type {import('next').NextConfig} */
 const withPWA = require('next-pwa');
 
+const __DEV__ = process.env.NODE_ENV !== 'production';
+
 const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['api.votely.mohammadalian.ir']
   },
   pwa: {
-    dest: 'public'
+    dest: 'public',
+    disable: __DEV__
   },
   optimizeFonts: false
 };
