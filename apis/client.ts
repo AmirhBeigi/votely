4ba17e1c5722b7402110df8c5ea0826c9f8662e0
Amirhossein Beigi <dev.amirhbeigi@gmail.com>
2022-04-29 12:01:23 +0430

    when not verify token redirect to location.pathname

--- apis/client.ts
+++ apis/client.ts
@@ -7,9 +7,10 @@ export const httpClient = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_BASE_URL
 });
 
+const excloudUrl = ['/auth/login', '/auth/register', '/auth/google-token/login', '/auth/refresh'];
+
 httpClient.interceptors.request.use(config => {
   const token = getCookie('votely.token');
-  const excloudUrl = ['/auth/refresh'];
   if (token && !excloudUrl.includes(config.url!)) {
     config.headers = { Authorization: `Bearer ${token}` };
   }
@@ -23,13 +24,6 @@ httpClient.interceptors.response.use(
   async error => {
     const originalRequest = error.config;
 
-    const excloudUrl = [
-      '/auth/login',
-      '/auth/register',
-      '/auth/google-token/login',
-      '/auth/refresh'
-    ];
-
     if (error.response?.status === 401 && !excloudUrl.includes(originalRequest.url)) {
       try {
         const { access_token } = await refresh();
@@ -39,7 +33,7 @@ httpClient.interceptors.response.use(
       } catch (error) {
         removeCookies('votely.token');
         removeCookies('votely.refresh_token');
-        return window.location.replace(`/login`);
+        return window.location.replace(window.location.pathname);
       }
     }
 
