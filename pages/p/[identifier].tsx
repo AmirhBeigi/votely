d8a5b90a7ba6c770b6b8d2284060b15c48e4b64c
Amirhossein <dev.amirhbeigi@gmail.com>
2022-08-12 13:54:42 +0430

    change url

--- pages/p/[identifier].tsx
+++ pages/p/[identifier].tsx
@@ -47,7 +47,7 @@ const Poll: NextPage<Props> = ({ error, poll }) => {
   );
   const [options, setOptions] = useState(poll?.options);
   const [isStoped, setIsStoped] = useState(poll?.is_closed);
-  const pollShareLink = `https://votely.vercel.app/p/${poll?.short_identifier}`;
+  const pollShareLink = `https://votely.zapdos.agency/p/${poll?.short_identifier}`;
 
   useEffect(() => {
     !isEmpty(user) && setIsUserOwnedPoll(poll?.owner_id === user.id);
